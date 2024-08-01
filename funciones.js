48acf0f77f09e173cf4b82ce669547ce4f4fd88b
oscarsaaaj <166893574+oscarsaaaj@users.noreply.github.com>
2024-07-31 23:41:04 -0600

    Add files via upload

---
+++ funciones.js
@@ -0,0 +1,9 @@
+$('#btn_guardar').click(function() {
+    Swal.fire({
+      title: 'Error!',
+      text: '¿Deseas continuar?',
+      icon: 'error',
+      confirmButtonText: 'Entendido'
+    });
+  });
+  
